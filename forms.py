0487b12f76ab2db13fcee8a9acc663ee4d41fb54
Tomant <tom.rgal@icloud.com>
2020-02-26 11:59:24 +0800

    test fix
    trying to fix github changes

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm, Form
+from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, IntegerField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
